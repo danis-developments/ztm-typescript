687a33d12fc162afd1aa1ff6b64de169311f148c
danis-developments <danis.developments@gmail.com>
2023-10-17 21:01:08 -0500

    Completed Switch statements

--- demo/switch/switch.ts
+++ demo/switch/switch.ts
@@ -3,3 +3,42 @@ import { strict as assert } from "assert";
 
 // Useful links:
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/switch
+
+let someNumber = parseInt("10");
+const sample = "sample";
+const someString = `${sample}`;
+
+switch (someString) {
+    case "test":
+        // code
+        break;
+    case "sample":
+        // code
+        break;
+    case "a":
+        // code
+        break;
+    default:
+        // some code
+}
+
+switch (someNumber) {
+    case 1:
+        break;
+    case 2:
+        break;
+    case 3:
+        break;
+    default:
+        //code 
+}
+
+switch (someNumber){
+    case 1:
+    case 2:
+    case 3:
+        //code
+    case 4:
+    case 5:
+        break;
+}
