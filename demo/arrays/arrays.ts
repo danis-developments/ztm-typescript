4dc833bb8fda7812c0c2ee1d4cf0c8db56de42b6
danis-developments <danis.developments@gmail.com>
2023-10-20 22:47:15 -0500

    updated ./demo/arrays/arrays.ts in order to complete arrays lesson.

--- demo/arrays/arrays.ts
+++ demo/arrays/arrays.ts
@@ -10,3 +10,40 @@ import { strict as assert } from "assert";
 //
 // Useful links:
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+
+const numbers: number[] = [1,2,3];
+let letters: string[] = ["a","b","c"];
+
+const b = letters [1];
+assert.equal(b,"b");
+
+letters[1] = "z";
+assert.equal(letters[1], "z");
+assert.deepEqual(letters,["a","z","c"]);
+
+// you can mix types, but this shouldn't be done unless it's a necessity
+const mixed = [1, "hello"];
+
+const names = [
+    ["Dani", "M"],
+    ["Alice","B"],
+]
+
+const dani = names [0][0];
+const M = names [0][1];
+
+assert.equal(dani, "Dani");
+assert.equal(M,"M");
+
+const nums: number[] = [];
+nums.push(10);
+nums.push(20);
+nums.push(30);
+assert.deepEqual(nums,[10,20,30]);
+
+nums.pop();
+assert.deepEqual(nums,[10,20]);
+
+nums.splice(0,1);
+assert.deepEqual(nums,[20]);
+
