bc23f7ad8ff3f8a3c3ee35a173517c76b271e75f
danis-developments <danis.developments@gmail.com>
2023-10-20 22:50:47 -0500

    updated ./demo/array-objects.ts in order to complete array-objects lesson.

--- demo/array-object/array-object.ts
+++ demo/array-object/array-object.ts
@@ -1,2 +1,25 @@
 /* eslint-disable */
 import { strict as assert } from "assert";
+
+type Link = {
+    title: string;
+    url: string;
+};
+
+const microsoft = {
+    title: "microsoft",
+    url: "microsoft.com",
+};
+
+const typescript = {
+    title: "TypeScript",
+    url: "typescriptlang.org",
+};
+
+const links = [microsoft, typescript];
+
+const tsURL = links[1].url;
+
+links[0].title = "Microsoft";
+
+
