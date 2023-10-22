c0ed8b6a9b03e215ac8ddb28f2e469d4c773f34c
danis-developments <danis.developments@gmail.com>
2023-10-21 22:19:06 -0500

    added ./app/todo/tods to complete Todo List: Initial Code lesson.

---
+++ app/todo/todo.ts
@@ -0,0 +1,39 @@
+import fs from "fs";
+
+const todosPath = "todos.json";
+
+type Todo = {
+    id: number;
+    task: string;
+}
+
+function getTodos(): Todo[] {
+    if(!fs.existsSync(todosPath)){
+        return [];
+    }
+
+    const data = fs.readFileSync(todosPath);
+    return JSON.parse(data.toString()) as Todo[];
+}
+
+function listTodos(): void {
+
+}
+
+function saveTodos(todos: Todo[]): void {
+    fs.writeFileSync(todosPath, JSON.stringify(todos));
+}
+
+function removeTodo(id: number): void {
+
+}
+
+function addTodo(task: string): void {
+    const todos: Todo[] = getTodos();
+    const id = todos.length > 0 ? todos[todos.length - 1].id + 1 : 1;
+    todos.push({ id, task });
+    saveTodos(todos);
+    console.log(`Added todo ${id}: ${task}`);
+}
+
+function cli(): void {}
