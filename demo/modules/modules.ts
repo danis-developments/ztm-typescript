30398dfd4b436c41d92b647e0345da2cedcc1ae2
danis-developments <danis.developments@gmail.com>
2023-10-21 22:05:39 -0500

    added ./demo/modules/math.ts, coord.ts and updated modules.ts in order to complete the modules lesson.

--- demo/modules/modules.ts
+++ demo/modules/modules.ts
@@ -1,9 +1,19 @@
 /* eslint-disable */
 import { strict as assert } from "assert";
 
+import {add, pi, Int as Integer} from "./math";
+import {Point} from "./coord";
+
 // ES modules provide a way to organize code into separate files that can be
 // imported and used in other files.To use an ES module, the the `import`
 // keyword is used.
 //
 // Useful links:
 // https://www.typescriptlang.org/docs/handbook/2/modules.html
+
+const three: Integer = 3;
+
+const four = add(2, 2);
+assert.equal(four, 4);
+
+
