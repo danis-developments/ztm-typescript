a4621c9f329037df0ddc511692856d82420159ef
danis-developments <danis.developments@gmail.com>
2023-10-22 20:58:57 -0500

    uptdated ./app/todo/todos.ts in order to complete Todo List: Add & List Todos lesson.

--- app/todo/todo.ts
+++ app/todo/todo.ts
@@ -17,6 +17,10 @@ function getTodos(): Todo[] {
 }
 
 function listTodos(): void {
+    const todos: Todo[] = getTodos();
+    for (let i = 0; i < todosPath.length; i++){
+        console.log(`${todos[i].id}: ${todos[i].task}`);
+    }
 
 }
 
@@ -25,8 +29,19 @@ function saveTodos(todos: Todo[]): void {
 }
 
 function removeTodo(id: number): void {
+    const todos: Todo[] = getTodos();
+    const index = todos.findIndex(function(todo){
+        return todo.id === id;
+    });
+    if (index === -1) {
+        console.log(`Colud not find todo with id ${id}`);
+        return;
+    }
 
-}
+    const removedTodo = todos.splice(index,1)[0];
+    saveTodos(todos);
+    console.log(`Removed todo ${removedTodo.id}: ${removedTodo.task}`);
+} 
 
 function addTodo(task: string): void {
     const todos: Todo[] = getTodos();
