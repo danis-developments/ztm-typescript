d1f20a2ffd64514a408ec05552808237c62be83c
danis-developments <danis.developments@gmail.com>
2023-10-24 22:17:10 -0500

    updated ./demo/iterators/iterator.js to complete Iterators lesson.

--- demo/iterators/iterators.ts
+++ demo/iterators/iterators.ts
@@ -8,3 +8,37 @@
 //
 // Useful links:
 // https://www.typescriptlang.org/docs/handbook/iterators-and-generators.html
+
+
+const abc = ["a","b","c"];
+
+//c-style loop
+for (let i= 0; i <abc.length; i++) {
+    console.log(abc[i]);
+}
+
+// using iterator
+
+for (const letter of abc) {
+    console.log(letter);
+}
+
+const nums = {
+    one: 1,
+    two: 2,
+    three: 3,
+    // b: "b", //if we define a string property here, typescript will know that this string type exsists and won't allow us to multiply the letter by 2
+};
+
+let property: keyof typeof nums;
+
+for (property in nums){
+    console.log(nums[property] * 2);
+}
+
+nums["letter"] = "a";
+
+// because nums["letter"] is introduced at runtime, typescript doesn't know that it shouldn't multiply it by 2, so you get NaN in the interface as opposed to a TypeScript compile error
+for (property in nums){
+    console.log(nums[property] * 2) ;
+}
