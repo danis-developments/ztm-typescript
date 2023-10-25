68714becbe0dbd891629408b3fa3f04af0ea51cc
danis-developments <danis.developments@gmail.com>
2023-10-24 22:35:34 -0500

    updated ./demo/maps/maps.ts to finish Working with Maps lesson

--- demo/maps/maps.ts
+++ demo/maps/maps.ts
@@ -11,3 +11,47 @@ import { strict as assert } from "assert";
 //
 // Useful links:
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map
+
+//unclear if we do it like this 
+// const testScores: Map<string, number> = new Map ();
+
+// so let's use type aliases
+
+type Name = string;
+type Score = number;
+
+const testScores: Map<Name,Score> = new Map();
+
+testScores.set("Alice", 96);
+testScores.set("Bob", 88);
+testScores.set("Carol", 92);
+
+assert.equal(testScores.size, 3);
+
+for (const [name, score] of testScores) {
+    console.log(`${name}, your score is ${score}.`);
+}
+
+testScores.delete("Bob");
+
+for (const [name, score] of testScores) {
+    console.log(`${name}, your score is ${score}.`);
+}
+
+assert.equal(testScores.size, 2);
+
+assert.equal(testScores.has("Bob"),false);
+
+console.log("Just the keys:");
+for (const name of testScores.keys()){
+    console.log(name);
+}
+
+console.log("Just the values")
+for (const value of testScores.values()){
+    console.log(value);
+}
+
+// to empty out the testScores map, use clear()
+testScores.clear();
+assert.equal(testScores.size, 0);
