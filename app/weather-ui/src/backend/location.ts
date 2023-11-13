fda772d7207556b8e09fa29a06d38b5355aa72ed
danis-developments <danis.developments@gmail.com>
2023-11-12 22:02:32 -0600

    updated ./app/weather-ui/src/backend/location.ts to complete Updating Location Functions lesson.

--- app/weather-ui/src/backend/location.ts
+++ app/weather-ui/src/backend/location.ts
@@ -1,12 +1,16 @@
-import axios from "axios";
+import axios, { AxiosStatic } from "axios";
+import { z } from "zod";
 
-export interface LocationInfo {
-    lat: string;
-    lon: string;
-    display_name: string;
-}
+const locationInfoSchema = z.object({
+    lat: z.string(),
+    lon: z.string(),
+    display_name: z.string(),
+});
+
+export type LocationInfo = z.infer<typeof locationInfoSchema>;
 
 export async function fetchLocationData (
+    axios: AxiosStatic,
     apiURL: string,
     locationName: string,
 ): Promise<LocationInfo> {
@@ -18,13 +22,14 @@ export async function fetchLocationData (
         }
     };
 
-    const response = await axios.request<LocationInfo[]>(options);
+    const response = await axios.request(options);
 
     if (response.status === 200) {
-        if (response.data.length >0){
-            return response.data[0];
-        } else {
-            throw new Error(`Unable to find location information`);
+        try {
+            return locationInfoSchema.parse(response.data[0]);
+        } catch (err) {
+            console.error(err);
+            throw new Error(`Unable to find location information.`);
         }
     } else {
         throw new Error("Failed to fetch location data");
